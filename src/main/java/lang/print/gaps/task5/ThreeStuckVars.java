1eaff6939fdf70389a987b8ea69b4b0fff82637b
naeliseev <elisnawork@gmail.com>
2024-10-17 14:43:53 +0300

    Done

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        // Создание и инициализация переменных
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        // Печать всех значений в одном сообщении
+        System.out.println(first + "" +  second + "" + third);
+
     }
 }
